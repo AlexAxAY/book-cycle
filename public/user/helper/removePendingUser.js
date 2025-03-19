2d7b58c1530a766a1193fb172047cd80cfec4e5c
Akshay Balachandran <alexaxay10619@gmail.com>
2025-03-19 11:13:30 +0530

    made changes to the pending user clear js file to ensure back button pressing reloads the page but the issue still persist for forward clicking

--- public/user/helper/removePendingUser.js
+++ public/user/helper/removePendingUser.js
@@ -1,3 +1,13 @@
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
+window.addEventListener("popstate", () => {
+  window.location.reload();
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   if (window.location.pathname !== "/user/verify-otp") {
     localStorage.removeItem("pendingUser");
